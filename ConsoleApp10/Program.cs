cf427901ad695188f3d15ca2e8bd8f7dd359548e
zekeriyajpg <104718308+zekeriyajpg@users.noreply.github.com>
2024-08-01 10:06:58 +0300

    Update Program.cs

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,35 +1,17 @@
-﻿using System;
+using Person;
 
-public class Person
-{
-    public string Ad { get; set; }
-    public string Soyad { get; set; }
-    public DateTime DogumTarihi { get; set; }
-
-    public Person(string ad, string soyad, DateTime dogumTarihi)
-    {
-        Ad = ad;
-        Soyad = soyad;
-        DogumTarihi = dogumTarihi;
-    }
-
-    public override string ToString()
-    {
-        return $"{Ad} {Soyad}, Doğum Tarihi: {DogumTarihi.ToShortDateString()}";
-    }
-}
 
 public class Program
 {
     public static void Main(string[] args)
     {
         // Öğrenci nesneleri oluşturuluyor
-        Person ogrenci1 = new Person("Ali", "Yılmaz", new DateTime(2003, 5, 21));
-        Person ogrenci2 = new Person("Ayşe", "Demir", new DateTime(2002, 8, 13));
+        oop ogrenci1 = new oop("Ali", "Yılmaz", new DateTime(2003, 5, 21));
+        oop ogrenci2 = new oop("Ayşe", "Demir", new DateTime(2002, 8, 13));
 
         // Öğretmen nesneleri oluşturuluyor
-        Person ogretmen1 = new Person("Ahmet", "Kaya", new DateTime(1975, 12, 19));
-        Person ogretmen2 = new Person("Fatma", "Şahin", new DateTime(1980, 3, 30));
+        oop ogretmen1 = new oop("Ahmet", "Kaya", new DateTime(1975, 12, 19));
+        oop ogretmen2 = new oop("Fatma", "Şahin", new DateTime(1980, 3, 30));
 
         // Öğrencilerin bilgilerini yazdırma
         Console.WriteLine("Öğrenciler:");
