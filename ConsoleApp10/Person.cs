f41d16519028f8e67b3ccb92438bfcbad6c0a039
zekeriyajpg <104718308+zekeriyajpg@users.noreply.github.com>
2024-08-01 10:08:42 +0300

    Add files via upload

---
+++ ConsoleApp10/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class oop
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+        public oop(string ad, string soyad, DateTime dogumTarihi)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            DogumTarihi = dogumTarihi;
+        }
+
+        public override string ToString()
+        {
+            return $"{Ad} {Soyad}, Doğum Tarihi: {DogumTarihi.ToShortDateString()}";
+        }
+    }
+}
